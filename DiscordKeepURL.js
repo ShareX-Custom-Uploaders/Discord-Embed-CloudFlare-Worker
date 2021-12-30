eff9afde1d57095c1817832cb7287c98e76dd4e7
Rhys <72263189+givinghawk@users.noreply.github.com>
2021-12-30 12:56:23 +0000

    Create DiscordKeepURL.js

---
+++ DiscordKeepURL.js
@@ -0,0 +1,44 @@
+async function handleRequest(request) {
+  const userAgent = request.headers.get("user-agent");
+  if (userAgent && userAgent.includes("Discord")) {
+    const response = await fetch(request);
+    if (response.status == 200) {
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.startsWith("image/")) {
+        return new Response(`<html>
+
+<head>
+
+  <meta property="twitter:card" content="summary_large_image">
+
+  <meta property="twitter:image" content="${request.url}">
+
+  <meta name="twitter:player" content="${request.url}">
+
+<meta name="twitter:player:height" content="360">
+
+<meta name="twitter:player:width" content="640">
+
+</head>
+
+</html>`, {
+
+          headers: {
+            "content-type": "text/html"
+          }
+        });
+      }
+    }
+    return response;
+  }
+  return fetch(request);
+}
+
+
+
+
+addEventListener("fetch", event => {
+
+  event.respondWith(handleRequest(event.request));
+
+});
